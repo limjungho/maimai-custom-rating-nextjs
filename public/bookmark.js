f603239747e1754393e0ffc875a7e90aef79e631
Jungho Lim <alex2007@naver.com>
2025-02-27 17:08:21 +0900

    Loading time increased 10s

--- public/bookmark.js
+++ public/bookmark.js
@@ -173,7 +173,7 @@ var fetchSequentially = async () => {
     console.error("Error api request :", error);
   }
   console.log("await INSERT query");
-  await sleep(8000);
+  await sleep(10000);
 
   alert("갱신이 완료되었습니다.\n확인을 클릭하면 INFO 화면으로 이동합니다.");
   window.open(URL + "/rating?friendcode=" + friendcode, "_blank"); // 새 탭에서 URL 열기
